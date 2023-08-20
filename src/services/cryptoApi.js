adf64bef4649c0bc9297a29a5bdb032d0cde14e2
Yssabel Pangilinan <yssabelpangilinan@yahoo.com>
2023-08-19 20:50:40 -0700

    added cryptoApi to redux store and added inital query logic

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,24 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+const cryptoApiHeaders = {
+    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
+    'X-RapidAPI-Key': '80210217ddmsh14e31a87a18c557p17d964jsnd687cd2bb387',
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders})
+
+export const cryptoApi = createApi({
+  reducerPath: 'cryptoApi',
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: () => createRequest('/coins')
+    })
+  })
+})
+
+export const {
+  useGetCryptosQuery,
+} = cryptoApi;
