803a85a4c4b03657e50a0fcb8bddb04f43fde570
Yssabel Pangilinan <yssabelpangilinan@yahoo.com>
2023-08-19 20:00:34 -0700

    adding routes and skeletons of individual components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Switch, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
 
-import { Navbar } from './components';
+import { Navbar, Exchanges, Homepage, Cryptocurrencies, News, CryptoDetails } from './components';
+import './App.css'
 
 const App = () => {
   return (
@@ -11,7 +12,17 @@ const App = () => {
         <Navbar />
       </div>
       <div className='main'>
-
+        <Layout>
+          <div className='routes'>
+            <Routes>
+              <Route exact path="/" element={<Homepage />}/>
+              <Route exact path="/exchanges" element={<Exchanges />}/>
+              <Route exact path="/cryptocurrencies" element={<Cryptocurrencies />}/>
+              <Route exact path="/crypto/:coinId" element={<CryptoDetails />}/>
+              <Route exact path="/news" element={<News />}/>
+            </Routes>
+          </div>
+        </Layout>
       </div>
       <div className='footer'>
 
